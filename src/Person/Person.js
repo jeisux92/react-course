71a8f2c51220deffdea1ef03e1b617fa97bb84d6
jeisux92 <jei.sum41@gmail.com>
2019-01-31 19:13:51 -0500

    Projection

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const person = (props) => {
-  return <p>I'm a person and I'm {props.age}</p>
-}
-
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm a {props.name} and I'm {props.age}
+      </p>
+      {props.children}
+    </div>
+  );
+};
 
 export default person;
