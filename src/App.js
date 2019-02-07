4c3d4dba421e382da6420114b6bd12717b43c21a
jeisux92 <jei.sum41@gmail.com>
2019-02-06 20:53:39 -0500

    handlers

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import Person from "./Person/Person";
 import "./App.css";
 
 class App extends Component {
+  changeNameHandler = () => {
+    this.setState((props, value) => ({
+      person: {
+        name: "Maximilian",
+        age: props.person.age + 24
+      }
+    }));
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      person: {
+        name: event.target.value,
+        age: 23
+      }
+    });
+  };
+
+  state = {
+    person: {
+      name: "Gabriel",
+      age: 26
+    }
+  };
   render() {
     return (
       <div className="App">
-        <Person name={"Gabriel"} age={23}>
-          <div>
-            <ul>
-              <li>a</li>
-              <li>b</li>
-              <li>c</li>
-              <li>d</li>
-            </ul>
-          </div>
-        </Person>
+        <button onClick={this.changeNameHandler.bind(this)} value="2">
+          Switch Name
+        </button>
+        <Person
+          change={this.nameChangedHandler.bind(this)}
+          click={e => this.changeNameHandler()}
+          name={this.state.person.name}
+          age={this.state.person.age}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,11 @@ import React from "react";
 const person = props => {
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         I'm a {props.name} and I'm {props.age}
       </p>
       {props.children}
+      <input type="text" onChange={props.change} value={props.name}></input>
     </div>
   );
 };
