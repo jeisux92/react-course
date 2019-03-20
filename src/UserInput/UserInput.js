2a9181ba23f6d6e6be13ba1527009f2c8c68b874
jeisux92 <jei.sum41@gmail.com>
2019-03-19 21:29:23 -0500

    conditional

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const userInput = (props) => <input onChange={props.change} value={props.name}></input>;
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const userOutput = (props) => (
+    <div>
+        <p>This is my user output</p>
+        <p>Holi {props.name}</p>
+    </div>
+);
+
+export default userOutput;
